16eb3911ab983b8391c1d480d94fa9b0c1ec4f65
datdudejibril <datdudejibril@gmail.com>
2017-05-11 09:26:45 -0400

    added header file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170511044146) do
+ActiveRecord::Schema.define(version: 20170511053230) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20170511044146) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
+    t.string   "reply_tweet"
   end
 
   create_table "users", force: :cascade do |t|
